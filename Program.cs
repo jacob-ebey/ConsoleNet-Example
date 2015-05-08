32a32f23569f4495f0f344144683e59661c915fb
Jacob Ebey <jacob.ebey@live.com>
2015-05-07 19:08:10 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using ConsoleNet;
+
+namespace ConsoleNetTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application app = new Application("ConsoleNetTest");
+
+            app.AddCommand(new FrameworkCommand("hello", SayHello)
+                { 
+                    Description = "Prints hello!",
+                    Help = "Option: --name <Name>"
+                });
+
+            app.Run(args);
+        }
+
+        static void SayHello(Application app)
+        {
+            string name;
+            app.TryGetParam("name", out name);
+
+            Console.WriteLine(string.Format("Hello, {0}!", name ?? app.Label));
+        }
+    }
+}
